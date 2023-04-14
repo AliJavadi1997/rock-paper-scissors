c828c944129c82a0ab93abd5966839d64e8ec892
AliJavadi1997 <alijavadi@Alis-MacBook-Air.local>
2023-04-14 17:36:48 +0200

    html modification

--- main.js
+++ main.js
@@ -1,132 +1,87 @@
 "use strict";
 
-/*
- *
- * makes a random choice form "rock", "paper" and "scissor"
- * @return {string} The random choice
- */
-let getComputerChoice = function () {
-  const options = ["rock", "paper", "scissor"];
-  return options[Math.floor(Math.random() * 3)];
-};
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
 
-/**
- *
- * @returns the user choice
- */
-let getPlayerChoice = function () {
-  const rock = document.querySelector("button.rock");
-  const paper = document.querySelector("button.paper");
-  const scissor = document.querySelector("button.scissor");
-  let userChoice;
-  rock.addEventListener("click", function () {
-    userChoice = "rock";
-  });
-  paper.addEventListener("click", function () {
-    userChoice = "paper";
-  });
-  scissor.addEventListener("click", function () {
-    userChoice = "scissor";
-  });
-  return userChoice;
-};
+function getComputerChoice() {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
 
-/**
- * @param {string} option
- * @return a DOM element that shows computer choice
- *
- */
-let showComputerChoice = function (option) {
-  const computerChoiceIcon = document.querySelector("div.computer-choice");
-  if (option == "rock") {
-    const rock = document.createElement("img");
-    rock.src = "./images/rock.png";
-    rock.style.width = "50px";
-    computerChoiceIcon.append(rock);
-  } else if (option == "paper") {
-    const paper = document.createElement("img");
-    paper.src = 'img src="./images/paper.png"';
-    paper.style.width = "50px";
-    computerChoiceIcon.append(paper);
-  } else {
-    const scissor = document.createElement("img");
-    scissor.src = 'img src="./images/scissor-svgrepo-com.svg"';
-    scissor.style.width = "50px";
-    computerChoiceIcon.append(scissor);
+function convertToWord(letter) {
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  if (letter === "s") return "Scissor";
+}
+
+function win(userChoice, computerChoice) {
+  userScore += 1;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "computer".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats
+  ${convertToWord(computerChoice)}${smallCompWord} you win! 🔥`;
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore += 1;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats 
+  ${convertToWord(computerChoice)}${smallCompWord} you lost! ❌`;
+}
+
+function draw(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals
+  ${convertToWord(computerChoice)}${smallCompWord} It's a draw! 🏳️`;
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "pr":
+    case "sp":
+    case "rs":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "sr":
+    case "ps":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "ss":
+    case "pp":
+      draw(userChoice, computerChoice);
+      break;
   }
-};
+}
 
-/**
- *
- * @param {string} userChoice
- */
-let showPlayerChoice = function (userChoice) {
-  const userChoiceIcon = document.querySelector("div.user-choice");
-  if (userChoice == "rock") {
-    const rock = document.createElement("img");
-    rock.innerHTML = 'img src="./images/rock.png"';
-    userChoiceIcon.append(rock);
-  } else if (userChoice == "paper") {
-    const paper = document.createElement("img");
-    paper.innerHTML = 'img src="./images/paper.png"';
-    userChoiceIcon.append(paper);
-  } else {
-    const scissor = document.createElement("img");
-    scissor.innerHTML = 'img src="./images/scissor-svgrepo-com.svg"';
-    userChoiceIcon.append(scissor);
-  }
-};
-
-let playerScore = 0;
-let computerScore = 0;
-/*
- *
- * @param {string} palyerSelection
- * @param {string} computerSelection
- * @returns if player or computer win a round
- */
-let playRound = function (palyerSelection, computerSelection) {
-  if (palyerSelection == "rock") {
-    if (computerSelection == "rock") {
-      playerScore += 0;
-    } else if (computerSelection == "paper") {
-      computerScore += 1;
-    } else if (computerSelection == "scissor") {
-      playerScore += 1;
-    }
-  } else if (palyerSelection == "paper") {
-    if (computerSelection == "paper") {
-      playerScore += 0;
-    } else if (computerSelection == "rock") {
-      playerScore += 1;
-    } else if (computerSelection == "scissor") {
-      computerScore += 1;
-    }
-  } else if (palyerSelection == "scissor") {
-    if (computerSelection == "scissor") {
-      playerScore += 0;
-    } else if (computerSelection == "rock") {
-      computerScore += 1;
-    } else if (computerSelection == "paper") {
-      playerScore += 1;
-    }
-  }
-  return playerScore;
-};
-
-let game = function () {
-  const startGame = document.querySelector("button.start-game");
-  startGame.addEventListener("click", function () {
-    for (let i = 0; i < 3; i++) {
-      let palyerSelection = getPlayerChoice();
-      showPlayerChoice(palyerSelection);
-      let computerSelection = getComputerChoice();
-      showComputerChoice(computerSelection);
-      playRound(palyerSelection, computerSelection);
-    }
+function main() {
+  rock_div.addEventListener("click", function () {
+    game("r");
   });
 
-  console.log(`Your score: ${playerScore}\nComputer score: ${computerScore}`);
-};
+  paper_div.addEventListener("click", function () {
+    game("p");
+  });
 
-game();
+  scissor_div.addEventListener("click", function () {
+    game("s");
+  });
+}
+
+main();
